3ad5446c3e32634961dd716ed4a010b2586d0b3f
tottiLap2 <rodrigoguerrerogarcia777futbol@gmail.com>
2025-10-03 20:53:53 -0600

    Commit inicial, agregando interfaz de login and signup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
+        
         <div className=" bg-orange-200 w-screen h-screen overflow-hidden grid" >
             <div className="place-content-center place-self-center place-items-center text-center">
                 <div className="flex flex-col items-center bottom-10 relative">
